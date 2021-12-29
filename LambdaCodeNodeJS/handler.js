c578ca6bd51c09b7d77be4d8e9d3565098952e31
Chinthana Pannila <chinthanapannila@gmail.com>
2021-12-29 21:05:49 +0530

    updated

--- LambdaCodeNodeJS/handler.js
+++ LambdaCodeNodeJS/handler.js
@@ -1,15 +1,5 @@
 'use strict';
 
 exports.studentService = async (event) => {
-    student = { name: "Johnx", age: 31, city: "New York1" };
-    return {statusCode: 200, body: JSON.stringify(student)}
-}
-
-function sendResponse(statusCode, message) {
-  const response = {
-    statusCode: statusCode,
-    body: JSON.stringify(message),
-    headers: {}
-  };
-  return response;
+    return {statusCode: 200, body: {}, headers: {}}
 }
