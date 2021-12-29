8153e68ac5352bc5006aeec2c6ce07f89a0da185
Chinthana Pannila <chinthanapannila@gmail.com>
2021-12-29 07:57:01 +0530

    student_handler

--- LambdaCode/student_lambda_function.py
+++ LambdaCode/student_lambda_function.py
@@ -1,6 +1,6 @@
 import json
 
-def lambda_handler(event, context):
+def student_handler(event, context):
     student = {"name": "John", "age": 30, "address": "Sri Lanka"}
     response = json.dumps(student)
     return {"statusCode": 200, "body": json.dumps(response)}
