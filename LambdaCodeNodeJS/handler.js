9b97aed3749fa11e4398acc54a9a087f7772335d
Chinthana Pannila <chinthanapannila@gmail.com>
2021-12-29 18:35:16 +0530

    updated code formatting.

--- LambdaCodeNodeJS/handler.js
+++ LambdaCodeNodeJS/handler.js
@@ -1,15 +1,15 @@
 'use strict';
 
-exports.modelSearchService = async (event) => {
+exports.studentService = async (event) => {
   students = [{"studentName": "test1"},{"studentName": "test2"},{"studentName": "test3"}];
   return sendResponse(200, JSON.stringify(students));
 }
 
 function sendResponse(statusCode, message) {
-	const response = {
-		statusCode: statusCode,
+  const response = {
+    statusCode: statusCode,
     body: JSON.stringify(message),
     headers: {}
-	};
-	return response;
+  };
+  return response;
 }
