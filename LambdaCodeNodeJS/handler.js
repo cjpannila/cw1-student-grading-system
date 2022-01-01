94a58364f7f4fb993150f3d206480470b3e41399
Chinthana Pannila <chinthanapannila@gmail.com>
2022-01-01 17:37:02 +0530

    updated to serve student service

--- LambdaCodeNodeJS/handler.js
+++ LambdaCodeNodeJS/handler.js
@@ -1,59 +1,32 @@
 'use strict';
 
 const databaseManager = require('./databaseManager');
-const https = require('https');
-let url = "https://docs.aws.amazon.com/lambda/latest/dg/welcome.html";
 
 exports.studentService =  function(event, context, callback) {
-  console.log("test message studentService");
-  https.get(url, (res) => {
-    callback(null, res.statusCode)
-  }).on('error', (e) => {
-    callback(Error(e))
-  })
+  console.log("test studentService");
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify("test success response"),
+    headers: {'Content-Type': 'application/json'}
+  };
+  callback(null, response);
 }
 
-exports.modelSearchService = async (event) => {
-	return getCarModel(event);
+exports.studentSearchService = async (event) => {
+  return getStudentModel(event);
 }
 
-exports.priceRetrievalService = async function (event){
-	return getCarPrice(event);
-}
-
-
-exports.optionsRetrievalService = async function(event){
-	return getCarOptions(event);
-}
-
-
-function getCarOptions(event) {
-	const itemId = event.pathParameters.itemId;
-	return databaseManager.getItem(itemId).then(response => {
-		console.log(response);
-		return sendResponse(200, JSON.stringify(response.options));
-	});
-}
-
-function getCarPrice(event) {
-	const itemId = event.pathParameters.itemId;
-	return databaseManager.getItem(itemId).then(response => {
-		console.log(response);
-		return sendResponse(200, JSON.stringify(response.price));
-	});
-}
-
-function getCarModel(event) {
-	const modelName = event.pathParameters.modelName;
-	return databaseManager.getAllCarModels().then(response => {
-		let item = null;
-		response.Items.forEach(element => {
-			if(element.name.toLowerCase() === modelName.toLowerCase()){
-				item = element;
-			}
-		});
-		return sendResponse(200, JSON.stringify(item));
-	});
+function getStudentModel(event) {
+  const studentName = event.pathParameters.studentName;
+  return databaseManager.getAllStudentModels().then(response => {
+    let item = null;
+    response.Items.forEach(element => {
+      if(element.name.toLowerCase() === studentName.toLowerCase()){
+        item = element;
+      }
+    });
+  return sendResponse(200, JSON.stringify(item));
+});
 }  
 
 function sendResponse(statusCode, message) {
