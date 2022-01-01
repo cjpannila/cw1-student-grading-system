1b48b9b8e40d61cf56f91f096a9d138e48eeed06
Chinthana Pannila <chinthanapannila@gmail.com>
2022-01-01 16:13:22 +0530

    Update handler.js

--- LambdaCodeNodeJS/handler.js
+++ LambdaCodeNodeJS/handler.js
@@ -6,6 +6,7 @@ const https = require('https');
 let url = "https://docs.aws.amazon.com/lambda/latest/dg/welcome.html";
 
 exports.studentService =  function(event, context, callback) {
+  console.log("test message studentService");
   https.get(url, (res) => {
     callback(null, res.statusCode)
   }).on('error', (e) => {
